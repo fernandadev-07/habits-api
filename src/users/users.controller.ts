928af54232c1ee2b2697da06948cfb783753e613
Heloisa <heloisagoncalves26@gmail.com>
2025-08-21 14:35:39 -0300

    ajustes prisma, GET e POST do usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+    // para criação dos usuários
+    @Post()
+    async createUser(@Body() body: { email: string; name: string }) {
+        return this.usersService.createUser(body);
+    }
+    // para retornar os usuários
+    @Get()
+    async getUsers() {
+        return this.usersService.getUsers();
+    }
+    // get de um usuário só
+    // deletar usuário
+    // editar usuário
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService){}
+    // para criar um usuário
+    async createUser(data: { email: string; name: string }) {
+        return this.prisma.user.create({ data });
+        // depois adicionar o relacionamento com os hábitos
+    }
+    // para acessar os usuários
+    async getUsers() {
+        return this.prisma.user.findMany();
+    }
+
+    // get de um usuário só
+    // deletar usuário
+    // editar usuário
+}
